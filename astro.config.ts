3559848cf4b06c702f881b3c80a445adc16d0686
Elian Van Cutsem <hello@elian.codes>
2024-02-06 23:04:51 +0000

    chore: add meta-tags inspector

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
+import metaTags from "astro-meta-tags";
 
 export default defineConfig({
-  site: 'https://kit.elian.codes',
+	site: "https://kit.elian.codes",
 	integrations: [
 		UnoCSS({
 			injectReset: true,
 		}),
 		solidJs(),
 		sitemap(),
+		metaTags(),
 	],
 });
