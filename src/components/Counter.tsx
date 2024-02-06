dab3b8c94964d55817d9896e826b2abe8e1d6581
Elian Van Cutsem <hello@elian.codes>
2024-02-06 22:27:37 +0000

    chore: lint

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,19 @@
-import { createSignal } from "solid-js"
+import { createSignal } from "solid-js";
 
 const Counter = () => {
-    const [count, setCount] = createSignal(0);
+	const [count, setCount] = createSignal(0);
 
-    return (
-        <>
-            <button type="button" onClick={() => setCount(count() + 1)}>
-                +
-            </button>
-            <p>{count()}</p>
-            <button type="button" onClick={() => setCount(count() - 1)}>
-                -
-            </button>
-        </>
-    )
-}
+	return (
+		<>
+			<button type="button" onClick={() => setCount(count() + 1)}>
+				+
+			</button>
+			<p>{count()}</p>
+			<button type="button" onClick={() => setCount(count() - 1)}>
+				-
+			</button>
+		</>
+	);
+};
 
 export default Counter;
