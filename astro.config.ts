f6929aba4219641d37d45674979a9080a20e4293
Elian Van Cutsem <hello@elian.codes>
2026-03-27 15:06:23 +0100

    feat: update to Astro 6

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from "astro/config";
-import UnoCSS from "unocss/astro";
-import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
-import metaTags from "astro-meta-tags";
+import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://kit.elian.codes",
-	integrations: [
-		UnoCSS({
-			injectReset: true,
-		}),
-		solidJs(),
-		sitemap(),
-		metaTags(),
-	],
+	integrations: [solidJs(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
